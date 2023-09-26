ec860cbe4b7cda47c3984c955597c62c975913ac
nsp1919 <110881782+nsp1919@users.noreply.github.com>
2023-09-26 16:16:55 +0530

    second

--- src/main/java/com/driver/Boat.java
+++ src/main/java/com/driver/Boat.java
@@ -4,14 +4,18 @@ public class Boat implements WaterVehicle{
     String name;
     int capacity;
 
+
     public Boat(String name, int capacity) {
         this.name = name;
         this.capacity = capacity;
     }
+
+    @Override
     public String getVehicleName(){
         return name;
     }
 
+    @Override
     public int getVehicleCapacity() {
         return capacity;
     }
